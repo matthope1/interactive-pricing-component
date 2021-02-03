054d2b57ecd50183308e9aee9038d6ae686f0d88
Matthew <matt-hope@hotmail.com>
2021-02-03 14:44:35 -0500

    created update slider function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 let slider = document.getElementById("slider");
+let body = document.getElementsByTagName("BODY")[0];
 
-slider.oninput = () => {
+function updateSliderBackground() {
     console.log("value: ", slider.value);
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
     slider.style.background = 'linear-gradient(to right,  hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%,hsl(224, 65%, 95%)' + value + '%,  hsl(224, 65%, 95%)';
+}
+
+slider.oninput = () => {
+    updateSliderBackground();
+}
+
+function pageLoaded() {
+    body.style.visibility = "visible";
+    updateSliderBackground();
 }
