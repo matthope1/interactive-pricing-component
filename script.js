968d0925d7546a7793683b3d70d4b2909f0c965e
Matthew <matt-hope@hotmail.com>
2021-02-03 13:59:30 -0500

    updated slider background color in slider function

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let slider = document.getElementById("slider");
 slider.oninput = () => {
     console.log("value: ", slider.value);
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
-    slider.style.background = 'linear-gradient(to right,  hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%, hsl(223, 50%, 87%)' + value + '%, hsl(223, 50%, 87%) 100%)';
+    slider.style.background = 'linear-gradient(to right,  hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%,hsl(224, 65%, 95%)' + value + '%,  hsl(224, 65%, 95%)';
 }
