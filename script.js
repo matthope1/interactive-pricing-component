7828892353f95452847c5fa54b528b1c5ddad7d6
Matthew <matt-hope@hotmail.com>
2021-02-03 19:44:52 -0500

    changed margin on card header and updated font size

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let slider = document.getElementById("slider");
 let body = document.getElementsByTagName("BODY")[0];
 
 function updateSliderBackground() {
-    console.log("value: ", slider.value);
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
     slider.style.background = 'linear-gradient(to right,  hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%,hsl(224, 65%, 95%)' + value + '%,  hsl(224, 65%, 95%)';
 }
