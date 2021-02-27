56c48a7bc3f2197fc05d5416b81403cae746044d
Matthew <matt-hope@hotmail.com>
2021-02-27 16:23:36 -0500

    added dynamic pricing

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
 let slider = document.getElementById("slider");
 let body = document.getElementsByTagName("BODY")[0];
-
-
-let priceDisplay = document.getElementById("price-display");
-
+let pageViewsDisplay = document.getElementById("page-views");
+let price = document.getElementById("price");
 
 function updateSliderBackground() {
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
@@ -22,27 +20,35 @@ function pageLoaded() {
 
 slider.onchange = (e) => {
     let value = e.target.value;
-    console.log(value);
     let pageViews;
-    // if value is smaller than 1/5
-    // then display 10k page views
-    // if the value 
 
     if (value >= 0 && value <= 20) {
         pageViews = "10K";
+        slider.value = 0;
+        price.innerHTML = "$8.00";
     }
     else if (value >= 21 && value <= 40){
         pageViews = "50K";
+        slider.value = 21;
+        price.innerHTML =  "$12.00";
     }
     else if (value >= 41 && value <= 60) {
         pageViews = "100K";
+        slider.value = 41;
+        price.innerHTML= "$16.00";
     }
     else if (value >= 61 && value <= 80) {
         pageViews = "500K";
+        slider.value = 61;
+        price.innerHTML= "$24.00";
     }
     else if (value >= 81 && value <= 100) {
         pageViews = "1M"
+        slider.value = 100;
+        price.innerHTML = "$36.00";
     }
-    priceDisplay.innerHTML = `${pageViews} PAGEVIEWS`;
+
+    updateSliderBackground();
+    pageViewsDisplay.innerHTML = `${pageViews} PAGEVIEWS`;
 
 }
